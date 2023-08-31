d540daa2e33f5ff909c8323b581893890e02e847
MunGyeongsam <mksday@gmail.com>
2023-08-31 22:49:09 +0900

    Chapter01 5. Graphical Editor
    5. Graphical Editor

--- algorithm/algorithmTest/jungol/Challenges/Chapter01.cs
+++ algorithm/algorithmTest/jungol/Challenges/Chapter01.cs
@@ -12,7 +12,8 @@ namespace jungol.Challenges
             //Prob01();
             //Prob02();
             //Prob03();
-            Prob04();
+            //Prob04();
+            Prob05();
         }
 
         //--------------------------------
@@ -127,9 +128,9 @@ namespace jungol.Challenges
 
                     if (map[r, c] == '*')
                     {
-                        ++t[tr - 1, tc - 1]; ++t[tr - 1, tc + 0];   ++t[tr - 1, tc + 1];
-                        ++t[tr - 0, tc - 1];                        ++t[tr - 0, tc + 1];
-                        ++t[tr + 1, tc - 1]; ++t[tr + 1, tc + 0];   ++t[tr + 1, tc + 1];
+                        ++t[tr - 1, tc - 1]; ++t[tr - 1, tc + 0]; ++t[tr - 1, tc + 1];
+                        ++t[tr - 0, tc - 1]; ++t[tr - 0, tc + 1];
+                        ++t[tr + 1, tc - 1]; ++t[tr + 1, tc + 0]; ++t[tr + 1, tc + 1];
                     }
                 }
             }
@@ -173,7 +174,7 @@ namespace jungol.Challenges
         {
             string[] lines = input.Split('\n');
             string line;
-            for(int i=0; i<lines.Length; ++i)
+            for (int i = 0; i < lines.Length; ++i)
             {
                 line = lines[i].Trim();
 
@@ -184,7 +185,7 @@ namespace jungol.Challenges
 
                 float[] arr = new float[n];
 
-                for(int j=0; j<n; ++j)
+                for (int j = 0; j < n; ++j)
                 {
                     line = lines[++i].Trim();
                     arr[j] = Convert.ToSingle(line);
@@ -235,7 +236,7 @@ namespace jungol.Challenges
         {
             string[] lines = input.Split('\n');
 
-            foreach(var line in lines)
+            foreach (var line in lines)
             {
                 string[] words = line.Split();
                 int s = Convert.ToInt32(words[0]);
@@ -317,14 +318,14 @@ namespace jungol.Challenges
 
 
             var horizontal = true;
-            for(int i=0; i<5; i++)
+            for (int i = 0; i < 5; i++)
             {
                 int N = horizontal ? 1 : s;
 
                 for (int j = 0; j < N; j++)
                 {
                     var n = 0;
-                    foreach(var c in num)
+                    foreach (var c in num)
                     {
                         n = c - '0';
 
@@ -361,6 +362,195 @@ namespace jungol.Challenges
         static void Prob04_Impl_DrawVertical(int s, bool flag1, bool flag2)
         {
             Console.Write(flag1 ? '|' : ' '); Console.Write(new string(' ', s)); Console.Write(flag2 ? '|' : ' ');
+        }
+
+        //--------------------------------
+        // 5. Graphical Editor
+        //--------------------------------
+        static void Prob05()
+        {
+            Prob05_Impl(@"I 5 6
+L 2 3 A
+S one.bmp
+G 2 3 J
+F 3 3 J
+V 2 3 4 W
+H 3 4 2 Z
+S two.bmp
+X");
+        }
+        static void Prob05_Impl(string input)
+        {
+            string[] lines = input.Split('\n');
+
+            char[,] img = null;
+
+            foreach (string line in lines)
+            {
+                string l = line.Trim();
+                char CMD = l[0];
+
+                switch (CMD)
+                {
+                    case 'I': img = Prob05_Impl_I(l); break;
+                    case 'C': Prob05_Impl_C(img); break;
+                    case 'L': Prob05_Impl_L(img, l); break;
+                    case 'V': Prob05_Impl_V(img, l); break;
+                    case 'H': Prob05_Impl_H(img, l); break;
+                    case 'K': Prob05_Impl_K(img, l); break;
+                    case 'F': Prob05_Impl_F(img, l); break;
+                    case 'S': Prob05_Impl_S(img, l); break;
+
+                    case 'X': return;
+                }
+            }
+        }
+        static void Prob05_Impl_Fill(char[,] img, char color, int x1 = -1, int y1 = -1, int x2 = -1, int y2 = -1)
+        {
+            int r1 = y1 < 0 ? 0 : y1 - 1;
+            int c1 = x1 < 0 ? 0 : x1 - 1;
+
+            int r2 = y2 < 0 ? img.GetLength(0) - 1 : y2 - 1;
+            int c2 = x2 < 0 ? img.GetLength(1) - 1 : x2 - 1;
+
+            int rstep = r1 < r2 ? 1 : -1;
+            int cstep = c1 < c2 ? 1 : -1;
+
+            for (int r = r1; ; r += rstep)
+            {
+                for (int c = c1; ; c += cstep)
+                {
+                    img[r, c] = color;
+                    if (c == c2) break;
+                }
+
+                if (r == r2) break;
+            }
+        }
+        static char[,] Prob05_Impl_I(string input)
+        {
+            string[] words = input.Split();
+            int M = Convert.ToInt32(words[1]);
+            int N = Convert.ToInt32(words[1]);
+
+            var img = new char[N, M];
+            Prob05_Impl_Fill(img, 'O');
+
+            return img;
+        }
+
+        static void Prob05_Impl_C(char[,] img)
+        {
+            Prob05_Impl_Fill(img, 'O');
+        }
+        static void Prob05_Impl_L(char[,] img, string input)
+        {
+            string[] words = input.Split();
+
+            int x = Convert.ToInt32(words[1]);
+            int y = Convert.ToInt32(words[2]);
+            char c = words[3][0];
+
+            img[y - 1, x - 1] = c;
+        }
+        static void Prob05_Impl_V(char[,] img, string input)
+        {
+            string[] words = input.Split();
+
+            int x = Convert.ToInt32(words[1]);
+            int y1 = Convert.ToInt32(words[2]);
+            int y2 = Convert.ToInt32(words[3]);
+            char c = words[4][0];
+
+            int step = y1 < y2 ? 1 : -1;
+
+            for (int y = y1; ; y += step)
+            {
+                img[y - 1, x - 1] = c;
+
+                if (y == y2)
+                    break;
+            }
+        }
+        static void Prob05_Impl_H(char[,] img, string input)
+        {
+            string[] words = input.Split();
+
+            int x1 = Convert.ToInt32(words[1]);
+            int x2 = Convert.ToInt32(words[2]);
+            int y = Convert.ToInt32(words[3]);
+            char c = words[4][0];
+
+            int step = x1 < x2 ? 1 : -1;
+
+            for (int x = x1; ; x += step)
+            {
+                img[y - 1, x - 1] = c;
+
+                if (x == x2)
+                    break;
+            }
+        }
+
+        static void Prob05_Impl_K(char[,] img, string input)
+        {
+            string[] words = input.Split();
+
+            int x1 = Convert.ToInt32(words[1]);
+            int y1 = Convert.ToInt32(words[2]);
+            int x2 = Convert.ToInt32(words[3]);
+            int y2 = Convert.ToInt32(words[4]);
+            char c = words[5][0];
+
+            Prob05_Impl_Fill(img, c, x1, y1, x2, y2);
+        }
+        static void Prob05_Impl_F(char[,] img, string input)
+        {
+            string[] words = input.Split();
+
+            int x = Convert.ToInt32(words[1]);
+            int y = Convert.ToInt32(words[2]);
+            char color = words[3][0];
+
+            bool[,] lookup = new bool[img.GetLength(0), img.GetLength(1)];
+
+            int r = y - 1;
+            int c = x - 1;
+
+            Prob05_Impl_F_Recv(img, lookup, r, c, img[r, c], color);
+        }
+        static void Prob05_Impl_F_Recv(char[,] img, bool[,] lookup, int r, int c, char org, char color)
+        {
+            if (r < 0 || r >= img.GetLength(0) || c < 0 || c >= img.GetLength(1))
+                return;
+
+            if (lookup[r, c])
+                return;
+
+            lookup[r, c] = true;
+            if (img[r, c] == org)
+            {
+                img[r, c] = color;
+
+                Prob05_Impl_F_Recv(img, lookup, r - 1, c, org, color);
+                Prob05_Impl_F_Recv(img, lookup, r + 1, c, org, color);
+                Prob05_Impl_F_Recv(img, lookup, r, c - 1, org, color);
+                Prob05_Impl_F_Recv(img, lookup, r, c + 1, org, color);
+            }
+        }
+        static void Prob05_Impl_S(char[,] img, string input)
+        {
+            string[] words = input.Split();
+            Console.WriteLine(words[1]);
+
+            for (int r = 0; r < img.GetLength(0); r++)
+            {
+                for (int c = 0; c < img.GetLength(1); c++)
+                {
+                    Console.Write(img[r, c]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
