5f33194650490336be18efb654f71d8972d7aedf
MunGyeongsam <mksday@gmail.com>
2023-09-15 17:07:40 +0900

    c# grammer inheritance

---
+++ algorithm/algorithmTest/jungol/LanguageCSharp/18_AccessModifier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace jungol.LanguageCSharp
+{
+
+    namespace Private_18_AccessModifier
+    {
+        class Base
+        {
+            private int _nv;
+            protected int _nt;
+            public int _np;
+        }
+
+        class Drived : Base
+        {
+            public void Test()
+            {
+                //_nv = 3;
+                _nt = 3;
+                _np = 4;
+            }
+        }
+    }
+    public class _8_AccessModifier
+    {
+        public static void Test()
+        {
+
+        }
+    }
+}
---
+++ algorithm/algorithmTest/jungol/LanguageCSharp/19_Inheritance.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace jungol.LanguageCSharp
+{
+    namespace Private_19
+    {
+        public class Animal
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        public class Dog : Animal
+        {
+            public void HowOld()
+            {
+                Console.WriteLine("나이: {0}", this.Age);
+            }
+        }
+
+        public class Bird : Animal
+        {
+            public void Fly()
+            {
+                Console.WriteLine("{0}가 날다", this.Name);
+            }
+        }
+
+
+
+        public abstract class PureBase
+        {
+            // abstract C#키워드 
+            public abstract int GetFirst();
+            public abstract int GetNext();
+        }
+
+        public class DerivedA : PureBase
+        {
+            private int no = 1;
+
+            // override C#키워드 
+            public override int GetFirst()
+            {
+                return no;
+            }
+
+            public override int GetNext()
+            {
+                return ++no;
+            }
+        }
+    }
+
+    public class _9_Inheritance
+    {
+        public _9_Inheritance()
+        {
+        }
+    }
+}
