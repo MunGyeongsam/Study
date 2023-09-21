b8153566c6a0dbbe81af1059b1c1523a6ea16ded
MunGyeongsam <mksday@gmail.com>
2023-09-21 14:00:31 +0900

    Update Chapter02_Prob010_PokerHands.cs

--- algorithm/algorithmTest/jungol/Challenges/Chapter02_Prob010_PokerHands.cs
+++ algorithm/algorithmTest/jungol/Challenges/Chapter02_Prob010_PokerHands.cs
@@ -28,6 +28,7 @@ namespace jungol.Challenges
                     case 'K': return 13;
                     case 'Q': return 12;
                     case 'J': return 11;
+                    case 'T': return 10;
                     default:
                         return n - '0';
                 }
@@ -64,9 +65,19 @@ namespace jungol.Challenges
     }
     class PockerHand
     {
-        enum TitleType
+        public enum TitleType
         {
+            HighCard,
+            OnePair,
+            TwoPair,
+            Triple,
+            Straight,
+            Flush,
+            FullHouse,
+            FourCard,
+            StraightFlush,
 
+            NUM
         }
         struct Counts
         {
@@ -75,6 +86,13 @@ namespace jungol.Challenges
             int Count3;
             int Count4;
             int Count5;
+
+            int Num1;
+            int Num2;
+            int Num3;
+            int Num4;
+            int Num5;
+
 
             public int this[int index]
             {
@@ -97,6 +115,47 @@ namespace jungol.Challenges
                         case 2: Count3 = value; break;
                         case 3: Count4 = value; break;
                         case 4: Count5 = value; break;
+                    }
+                }
+            }
+
+            public int this[int i, int j]
+            {
+                get
+                {
+                    switch (i)
+                    {
+                        case 0: return (j==0) ? Count1 : Num1;
+                        case 1: return (j==0) ? Count2 : Num2;
+                        case 2: return (j==0) ? Count3 : Num3;
+                        case 3: return (j==0) ? Count4 : Num4;
+                        case 4: return (j==0) ? Count5 : Num5;
+                    }
+                    return -1;
+                }
+                set
+                {
+                    if (j == 0)
+                    {
+                        switch (i)
+                        {
+                            case 0: Count1 = value; break;
+                            case 1: Count2 = value; break;
+                            case 2: Count3 = value; break;
+                            case 3: Count4 = value; break;
+                            case 4: Count5 = value; break;
+                        }
+                    }
+                    else
+                    {
+                        switch (i)
+                        {
+                            case 0: Num1 = value; break;
+                            case 1: Num2 = value; break;
+                            case 2: Num3 = value; break;
+                            case 3: Num4 = value; break;
+                            case 4: Num5 = value; break;
+                        }
                     }
                 }
             }
@@ -123,7 +182,115 @@ namespace jungol.Challenges
 
         public override string ToString()
         {
-            return string.Format($"{_cards[0]} {_cards[1]} {_cards[2]} {_cards[3]} {_cards[4]}");
+            return string.Format($"{_cards[0]} {_cards[1]} {_cards[2]} {_cards[3]} {_cards[4]} : {Title}");
+        }
+
+        public int Cmp(PockerHand rhs)
+        {
+            var t1 = Title;
+            var t2 = rhs.Title;
+
+            if (t1 == t2)
+            {
+                switch(t1)
+                {
+                    case TitleType.StraightFlush:
+                    case TitleType.FourCard:
+                    case TitleType.Straight:
+                    case TitleType.FullHouse:
+                    case TitleType.Triple:
+                        return _cards[2].Number - rhs._cards[2].Number;
+
+                    case TitleType.Flush:
+                    case TitleType.HighCard:
+                        for (var i=4; i>=0; --i)
+                        {
+                            var diff = _cards[i].Number - rhs._cards[i].Number;
+                            if (0 == diff)
+                                continue;
+
+                            return diff;
+                        }
+                        return 0;
+
+                    case TitleType.TwoPair:
+                        break;
+
+                    case TitleType.OnePair:
+                        break;
+
+                    default: throw new IndexOutOfRangeException();
+                }
+            }
+            else
+                return t1 - t2;
+
+            return 0;
+        }
+
+        int FirstPairNumber
+        {
+            get
+            {
+                Counts cnt = Counting;
+                for (var i = 0; i < 5; ++i)
+                    if (cnt[i] == 2)
+                        return cnt[i,1];
+                return -1;
+            }
+        }
+
+        int SecondPairNumber
+        {
+            get
+            {
+                bool isFirst = true;
+                Counts cnt = Counting;
+                for (var i = 0; i < 5; ++i)
+                {
+                    if (cnt[i] == 2)
+                    {
+                        if (isFirst)
+                            isFirst = false;
+                        else
+                            return cnt[i, 1];
+                    }
+                }
+                return -1;
+            }
+        }
+        //int 
+
+        public TitleType Title
+        {
+            get
+            {
+                if (IsStraightFlush)
+                    return TitleType.StraightFlush;
+
+                if (IsFourCard)
+                    return TitleType.FourCard;
+
+                if (IsFullHouse)
+                    return TitleType.FullHouse;
+
+                if (IsFlush)
+                    return TitleType.Flush;
+
+                if (IsStraight)
+                    return TitleType.Straight;
+
+                if (IsTriple)
+                    return TitleType.Triple;
+
+                if (IsTwoPair)
+                    return TitleType.TwoPair;
+
+                if (IsOnePair)
+                    return TitleType.OnePair;
+
+                return TitleType.HighCard;
+            }
         }
 
         public bool IsStraightFlush
@@ -140,12 +307,15 @@ namespace jungol.Challenges
                 int n = _cards[0].Number;
                 int index = 0;
                 count[index] = 1;
+                count[index, 1] = n;
                 for(int i=1; i<5; ++i)
                 {
                     if (n != _cards[i].Number)
                     {
+                        n = _cards[i].Number;
                         index++;
                         count[index] = 1;
+                        count[index, 1] = n;
                     }
                     else
                     {
@@ -157,23 +327,61 @@ namespace jungol.Challenges
             }
         }
 
+        public bool IsOnePair
+        {
+            get
+            {
+                var cnts = Counting;
+                int rt = 0;
+                for (var i = 0; i < 5; ++i)
+                {
+                    if (cnts[i] == 0)
+                        break;
+                    if (cnts[i] == 2)
+                        ++rt;
+                }
+
+                return rt == 1;
+            }
+        }
+
+        public bool IsTwoPair
+        {
+            get
+            {
+                var cnts = Counting;
+                int rt = 0;
+                for(var i=0; i<5; ++i)
+                {
+                    if (cnts[i] == 0)
+                        break;
+                    if (cnts[i] == 2)
+                        ++rt;
+                }
+
+                return rt == 2;
+            }
+        }
+
+        public bool IsTriple
+        {
+            get
+            {
+                var cnts = Counting;
+                for(var i=0; i<5; ++i)
+                {
+                    if (cnts[i] == 3)
+                        return true;
+                }
+                return false;
+            }
+        }
         public bool IsFourCard
         {
             get
             {
-                int n1 = _cards[0].Number;
-                int n2 = _cards[1].Number;
-
-                int cnt = (n1 == n2) ? 2 : 1;
-                for(int i=2; i<5; ++i)
-                {
-                    if (_cards[i].Number == n2)
-                        ++cnt;
-                    else
-                        break;
-                }
-
-                return cnt == 4;
+                var cnts = Counting;
+                return cnts[0] == 4 || cnts[1] == 4;
             }
         }
 
@@ -181,7 +389,18 @@ namespace jungol.Challenges
         {
             get
             {
-                return false;
+                var rt = true;
+                var cnts = Counting;
+
+                for (int i = 0; i < 5; ++i)
+                {
+                    if (cnts[i] < 2)
+                    {
+                        rt = false;
+                        break;
+                    }
+                }
+                return rt;
             }
         }
 
@@ -232,6 +451,12 @@ namespace jungol.Challenges
                 string[] words = line.TrimEnd().Split();
                 B.Set(words[0], words[1], words[2], words[3], words[4]);
                 W.Set(words[5], words[6], words[7], words[8], words[9]);
+
+                var rt = B.Cmp(W);
+
+                Console.WriteLine(B);
+                Console.WriteLine(W);
+                Console.WriteLine(rt > 0 ? "Black Win" : (rt < 0 ? "White Win" : "Tie"));
             }
 
         }
