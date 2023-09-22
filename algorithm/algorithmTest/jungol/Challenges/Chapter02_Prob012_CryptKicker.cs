4b9b59a83f38522d46b033fbe908a03526ed69ba
MunGyeongsam <mksday@gmail.com>
2023-09-22 22:54:09 +0900

    Chapter02_Prob012_CryptKicker
    Chapter02_Prob012_CryptKicker

---
+++ algorithm/algorithmTest/jungol/Challenges/Chapter02_Prob012_CryptKicker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jungol.Challenges
+{
+    internal class Chapter02_Prob012_CryptKicker
+    {
+        public static void Run()
+        {
+            Solve(@"6
+and
+dick
+jane
+puff
+spot
+yertle
+bjvg xsb hxsn xsb gymm xsb rqat xsb pnetfn
+xxxx yyy zzzz www yyyy aaa bbbb ccc dddddd");
+        }
+
+        private static void Solve(string input)
+        {
+
+        }
+    }
+}
